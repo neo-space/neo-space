e6a17f825e9f0c63c7922a35d6fc6121c4669aa5
spiralClouds <100466805+spiralClouds@users.noreply.github.com>
2024-08-21 19:59:30 -0400

    refactor: moved practice and testing wasm and rendering projects into rust-packages directory

---
+++ rust-packages/hello-wasm/README.md
@@ -0,0 +1,8 @@
+This is a simple sanity check
+---
+###### running the project
+in this directory run
+```bash
+npm run serve
+```
+that will run `webpack` and compile the Rust lib into binaries and insert them into `/pkg`
--- rust-packages/hello-wasm/src/lib.rs
+++ rust-packages/hello-wasm/src/lib.rs
@@ -1,8 +1,11 @@
 
 use wasm_bindgen::prelude::*;
 
+// this macro generates the necessary boilerplate to allow
+// the rust function to be called from boilerplate
 #[wasm_bindgen]
 extern "C" {
+    // this allows Rust to call the Javascript alert function
     fn alert(s: &str);
 }
 
