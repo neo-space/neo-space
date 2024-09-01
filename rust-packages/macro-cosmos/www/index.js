f25f6b6748ac7a5c0e1bf87406d1072764f7244c
spiralClouds <100466805+spiralClouds@users.noreply.github.com>
2024-09-01 12:44:31 -0400

    feat: mouse event listeners in www/index.js paper grid in www/index.html

--- rust-packages/macro-cosmos/www/index.js
+++ rust-packages/macro-cosmos/www/index.js
@@ -1,8 +1,34 @@
 
-import init from '../pkg/macro_cosmos.js';
+import init, { Renderer } from '../pkg/macro_cosmos.js';
 
 async function run() {
-  await init();
+    await init();
+    const renderer = new Renderer();
+    
+    let isDrawing = false;
+    let startX, startY;
+
+    const canvas = document.getElementById('canvas');
+    const rect = canvas.getBoundingClientRect();
+
+    function getNormalizedCoords(clientX, clientY) {
+        const x = (clientX - rect.left) / rect.width * 2 - 1;
+        const y = -((clientY - rect.top) / rect.height * 2 - 1);
+        return [x, y];
+    }
+
+    canvas.addEventListener('mousedown', (e) => {
+        isDrawing = true;
+        [startX, startY] = getNormalizedCoords(e.clientX, e.clientY);
+    });
+
+    canvas.addEventListener('mouseup', (e) => {
+        if (isDrawing) {
+            const [endX, endY] = getNormalizedCoords(e.clientX, e.clientY);
+            renderer.draw_square(startX, startY, endX, endY);
+            isDrawing = false;
+        }
+    });
 }
 
 run();
